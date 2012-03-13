312e04898671ca8f7fa1ab1d269915c27fda7ff2
Bala Subramanian <mccbala@gmail.com>
2012-03-13 20:19:59 +0530

    ./rip2mp4mkv/ - contains interactive & commandline-run windows batch scripts

---
+++ rip2mp4mkv/irip2mp4.bat
@@ -0,0 +1,43 @@
+@echo off
+set org=%1
+set base=""
+set vtime=""
+set atime=""
+set /p width=ENTER FRAME WIDTH FOR VIDEO: 
+set /p height=ENTER FRAME HEIGHT FOR VIDEO: 
+set /p vqual=ENTER QUALITY FOR VIDEO [0(lossless) to 51(least), 23 is normal]: 
+set /p aqual=ENTER BITRATE FOR AUDIO IN kbps [64-320, 128 is normal]: 
+IF /I %2 EQU yes ( 
+	set base="%org:~1,-5%-sample
+	set vtime=--frames 1800
+	set atime=-t 60
+echo ==========NOTE: SAMPLE WILL BE OF 1 MINUTE DURATION APPROXIMATELY.===========
+) ELSE (
+	set base="%org:~1,-5%
+	set vtime= 
+	set atime= 
+)
+set avs=%base%-avs.avs"
+set vid=%base%-vid.mp4"
+set aud=%base%-aud.aac"
+set rip=%base%-iPod-x264-AAC.mp4"
+IF EXIST %vid% del /q %vid%
+IF EXIST %aud% del /q %aud%
+IF EXIST %avs% del /q %avs%
+IF EXIST %rip% del /q %rip%
+echo =============================================================================
+echo ==========================STARTING VIDEO ENCODING============================
+echo =============================================================================
+echo spline64resize(directshowsource(%org%),%width%,%height%) >> %avs%
+x264 --level 3.0 --profile main --crf %vqual% %vtime% -o %vid% %avs%
+echo =============================================================================
+echo ==========================STARTING AUDIO ENCODING============================
+echo =============================================================================
+ffmpeg -i %avs% -vn -ab %aqual%k -y %atime% -ac 2 -ar 44100 %aud%
+echo =============================================================================
+echo ============================MUXING VIDEO & AUDIO=============================
+echo =============================================================================
+mp4box -out %rip% %vid% -add %aud% -ipod
+del %vid%
+del %aud%
+del %avs%
